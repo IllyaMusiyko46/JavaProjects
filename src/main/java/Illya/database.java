eaf18348c5c624ee1260d5e0317d2045d136f383
illya <illya.musiyko@gmail.com>
2022-05-02 20:39:28 +0300

    my first commit

---
+++ src/main/java/Illya/database.java
@@ -0,0 +1,56 @@
+package Illya;
+
+import com.sun.tools.javac.Main;
+
+import java.sql.*;
+import java.util.logging.*;
+import java.sql.Statement;
+
+public class database {
+
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            String userName = "root";
+            String password = "illya46";
+            String url = "jdbc:MySQL://localhost/languages";
+            conn = DriverManager.getConnection(url, userName, password);
+            System.out.println("Database connected ");
+        } catch (Exception ex) {
+            System.err.println("Cannot connect to database server");
+            ex.printStackTrace();
+        }
+
+        // get values from database
+
+        Statement st = conn.createStatement();
+        String sql = ("SELECT * FROM `languages` LIMIT 50");
+        ResultSet rs = st.executeQuery(sql);
+        if (rs.next()) {
+           System.out.println(rs.getString("PHP"));
+
+
+        }
+
+        // insert valuers to database
+String query = ("INSERT INTO languages + values(?,?,?,?,?)");
+        PreparedStatement preparedstmt = conn.prepareStatement(query);
+        preparedstmt.setString(1,"REREW");
+        preparedstmt.setString(2,"rerwrwrwrwr");
+        preparedstmt.setString(3,"fdsfsdfe");
+        preparedstmt.setString(4,"fdsfsfsfsfwwwrw");
+
+
+
+    }
+
+}
+
+
+
+
+
